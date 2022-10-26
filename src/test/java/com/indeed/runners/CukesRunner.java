ecd77f007c317627f01cdf7a0928f7081c758436
Muhammet Muaz <m.muazyagci@gmail.com>
2022-10-26 20:31:10 +0200

    Second Commit Creating Feature file for the Project

---
+++ src/test/java/com/indeed/runners/CukesRunner.java
@@ -0,0 +1,23 @@
+package com.indeed.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin ={
+                //"pretty",
+                "html:target/cucumber-reports.html",
+                "rerun:target/rerun.txt",
+                "me.jvt.cucumber.report.PrettyReports:target/cucumber"
+        } ,
+        features = "src/test/resources/features",
+        glue = "com/cydeo/step_definitions",
+        tags = "",
+        publish = true
+
+
+)
+public class CukesRunner {
+}
