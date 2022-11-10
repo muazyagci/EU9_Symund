7e1ac4a2574aeb2d93df6a17c421814c3f9713c5
Muhammet Muaz <m.muazyagci@gmail.com>
2022-11-10 20:06:07 +0100

    Day2

--- src/test/java/com/Symund/runners/CukesRunner.java
+++ src/test/java/com/Symund/runners/CukesRunner.java
@@ -14,7 +14,7 @@ import org.junit.runner.RunWith;
 //                "me.jvt.cucumber.report.PrettyReports:target/cucumber"
         } ,
         features = "src/test/resources/features",
-        glue = "com/Symund/step_definitions",
+        glue = "com/Symund/step_definitions"
 
 
 
