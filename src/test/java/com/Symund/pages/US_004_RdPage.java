d34dc343fa301cf458efb900ba5f6fc4ef0b98b5
Rahman16-dev <demirkolrd1@gmail.com>
2022-11-18 14:29:58 +0100

    Some step definitions

--- src/test/java/com/Symund/pages/US_004_RdPage.java
+++ src/test/java/com/Symund/pages/US_004_RdPage.java
@@ -8,6 +8,10 @@ public class US_004_RdPage extends BasePage {
     @FindBy (xpath = "(//a[@class= 'action action-menu permanent'])[4]")
     public WebElement threeDotsReadMe;
 
+    //*[@id="fileList"]/tr[4]/td[2]/a/span[2]/a[2]
+
+    @FindBy(xpath = )
+
 
 
 }
